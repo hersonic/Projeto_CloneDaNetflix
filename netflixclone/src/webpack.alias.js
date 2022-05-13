c34ae87cd49476cca88c1732f4b19868735f45e0
hersonic <hersonic82@gmail.com>
2022-05-13 18:25:42 -0300

    Finalização de projeto clone react iniciante

---
+++ netflixclone/src/webpack.alias.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //...
+ module.exports = {
+    //...
+ module.exports = {
+   //...
+ resolve: {
+     alias: {
+       '@material-ui/styled-engine': '@material-ui/styled-engine-sc',
+     },
+   },
+ };
